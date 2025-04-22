a932cb57cd72ed729f03798284dd49b9ba286368
Jishnu Saurav Mittapalli <55013611+jishnusaurav@users.noreply.github.com>
2025-04-22 11:15:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ loss=tf.keras.losses.BinaryCrossentropy()
 loss
 
 # Load the trained model
-model = tf.keras.models.load_model('model.h5',loss=loss)
+model = tf.keras.models.load_model('model.h5',custom_objects={'my_loss': loss})
 
 # Load the encoders and scaler
 with open('label_encoder_gender.pkl', 'rb') as file:
